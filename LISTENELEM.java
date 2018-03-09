591581d1e6c12d7027c2fb834a092af0ded40a2a
AlTrAugsburg <34038755+AlTrAugsburg@users.noreply.github.com>
2018-03-09 19:43:15 +0100

    Update LISTENELEM.java

--- LISTENELEM.java
+++ LISTENELEM.java
@@ -3,7 +3,7 @@ abstract class LISTENELEM {
     abstract LISTENELEM NächsterGeben();
     abstract DATENELEM InhaltGeben();
     abstract void NächsterSetzen(LISTENELEM le);
-    abstract LISTENELEM LetztenKnotenLöschen(LISTENELEM vorvorgänger);
+    abstract LISTENELEM LetztenKnotenLöschen();
     abstract LISTENELEM LetztenKnotenGeben(LISTENELEM vorgänger);
     abstract int AnzahlGeben();
     abstract boolean IstListeLeer();
