d435b8181d071e0572d02088c916c0a2f5fa241e
AlTrAugsburg <34038755+AlTrAugsburg@users.noreply.github.com>
2018-03-09 07:15:55 +0100

    Create DATENELEM.java
    Class for the data

---
+++ DATENELEM.java
@@ -0,0 +1,13 @@
+//Datenelements Klasse
+public class DATENELEM {
+    private String begriff;
+    
+    DATENELEM(String begriffNeu) {
+        begriff = begriffNeu;
+    }
+    
+    String BegriffGeben() {
+        return begriff;
+    }
+
+}
