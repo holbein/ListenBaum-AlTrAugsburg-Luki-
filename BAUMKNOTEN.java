719d20e9432e728cec4ea9b0e94074672a6e354b
AlTrAugsburg <34038755+AlTrAugsburg@users.noreply.github.com>
2018-03-11 12:07:19 +0100

    Update BAUMKNOTEN.java
    Now working

--- BAUMKNOTEN.java
+++ BAUMKNOTEN.java
@@ -15,7 +15,8 @@ class BAUMKNOTEN extends BAUMELEM {
     }
     
     BAUMELEM RechtsGeben(){
-        return rechts;}
+        return rechts;
+    }
         
     DATENELEM InhaltGeben(){
         return inhalt;
@@ -27,6 +28,32 @@ class BAUMKNOTEN extends BAUMELEM {
     }
         
     BAUMKNOTEN(LISTE l){
-        
+        if(l.LängeGeben()==1){
+            inhalt = l.ErsterGeben().InhaltGeben();
+            links = new BAUMABSCHLUSS();
+            rechts = new BAUMABSCHLUSS();
+        }
+        else{
+            if(l.LängeGeben()==2){
+                inhalt = l.LetztenKnotenGeben().InhaltGeben();
+                LISTE liste = new LISTE();
+                liste.HintenEinfügen(l.ErsterGeben().InhaltGeben());
+                links = new BAUMKNOTEN(liste);
+                rechts = new BAUMABSCHLUSS();
+            }
+            else{
+                inhalt = l.MitteGeben().InhaltGeben();
+                links = new BAUMKNOTEN(l.LinkeHälfteTrennen());
+                rechts = new BAUMKNOTEN(l.RechteHälfteTrennen());
+            }
+        }
+    }
+    
+    void InOrderAusgeben(){
+        links.InOrderAusgeben();
+        System.out.println("[");
+        System.out.println(inhalt.BegriffGeben());
+        System.out.println("]");
+        rechts.InOrderAusgeben();
     }
 }
