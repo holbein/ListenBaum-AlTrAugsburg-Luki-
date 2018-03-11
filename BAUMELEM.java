2dfc9ca9491b9293f7d90c2bdcb1366e4b09f09a
AlTrAugsburg <34038755+AlTrAugsburg@users.noreply.github.com>
2018-03-11 12:06:46 +0100

    Update BAUMELEM.java
    Now working

--- BAUMELEM.java
+++ BAUMELEM.java
@@ -1,8 +1,8 @@
-//Oberklasse von Baumknoten und Baumabschluss
 abstract class BAUMELEM {
 
     abstract BAUMELEM LinksGeben();
     abstract BAUMELEM RechtsGeben();
     abstract DATENELEM InhaltGeben();
     abstract int AnzahlKnotenBerechnen();
+    abstract void InOrderAusgeben();
 }
